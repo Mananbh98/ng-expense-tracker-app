734b191a90ab52a6d3d31fe4c7308a937278aab9
Manan <mananbhanushali98@gmail.com>
2020-12-03 03:55:40 +0530

    Adding new cards based on items added

--- src/app/budget-item-list/budget-item-list.component.ts
+++ src/app/budget-item-list/budget-item-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { BudgetItem } from './../../shared/models/budget-item.model';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-budget-item-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budget-item-list.component.scss']
 })
 export class BudgetItemListComponent implements OnInit {
-
+  @Input() budgetItems: BudgetItem[];
   constructor() { }
 
   ngOnInit(): void {
