04ac0405036b465228486d3d2eec172f84f974a2
Manan <mananbhanushali98@gmail.com>
2020-12-03 21:23:41 +0530

    Added dynamic display for total budget display

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,27 +1,36 @@
 import { BudgetItem } from './../../shared/models/budget-item.model';
 import { Component, OnInit } from '@angular/core';
+import { UpdateEvent } from '../budget-item-list/budget-item-list.component';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.scss']
+  styleUrls: ['./main-page.component.scss'],
 })
 export class MainPageComponent implements OnInit {
-
   budgetItems: BudgetItem[] = new Array<BudgetItem>();
+  totalBudget: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  addItem(newItem: BudgetItem){
+  addItem(newItem: BudgetItem) {
     this.budgetItems.push(newItem);
-
+    this.totalBudget += newItem.amount;
   }
-  deleteItem(item: BudgetItem){
+  deleteItem(item: BudgetItem) {
     let index = this.budgetItems.indexOf(item);
-    this.budgetItems.splice(index,1);
+    this.budgetItems.splice(index, 1);
+    this.totalBudget -= item.amount;
   }
+  updateItem(updateEvent: UpdateEvent) {
+    // replace the item with updated/submitted item form
+    this.budgetItems[this.budgetItems.indexOf(updateEvent.old)] =
+      updateEvent.new;
 
+    // update the total budget
+    this.totalBudget -= updateEvent.old.amount;
+    this.totalBudget += updateEvent.new.amount;
+  }
 }
