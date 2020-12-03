2cc736c2f984080ff5e75caeadc4841c077dc0ea
Manan <mananbhanushali98@gmail.com>
2020-12-03 20:58:39 +0530

    Added card values can be edited

--- src/app/add-item-form/add-item-form.component.ts
+++ src/app/add-item-form/add-item-form.component.ts
@@ -1,27 +1,36 @@
 import { BudgetItem } from './../../shared/models/budget-item.model';
-import { Component, OnInit,Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-item-form',
   templateUrl: './add-item-form.component.html',
-  styleUrls: ['./add-item-form.component.scss']
+  styleUrls: ['./add-item-form.component.scss'],
 })
 export class AddItemFormComponent implements OnInit {
-  @Input() item: BudgetItem = new BudgetItem('', null);
-   /*by default there wont be anything inside the form so for now we are adding empty stringa nd null amount
-*/
-  @Output() formSubmit: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
+  @Input() item: BudgetItem;
+  /*by default there wont be anything inside the form so for now we are adding empty stringa nd null amount
+   */
+  @Output()
+  formSubmit: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
 
-  constructor() { }
+  isNewItem: boolean;
+  constructor() {}
 
   ngOnInit(): void {
+    //if item has a value
+    if (this.item) {
+      //This means that an existing item object was passed into this component
+      //Thus this is not a new item
+      this.isNewItem = false;
+    } else {
+      this.isNewItem = true;
+      this.item = new BudgetItem('', null);
+    }
   }
 
-  onSubmit(form: NgForm){
+  onSubmit(form: NgForm) {
     this.formSubmit.emit(form.value);
     form.reset();
-
   }
-
 }
